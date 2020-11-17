d51a6cdc4279052de49de7b9756df9ed0c4d729b
Marcelo <marcelo.jara@pucp.edu.pe>
2020-11-17 15:10:50 -0500

    Done

---
+++ main.py
@@ -0,0 +1,69 @@
+#Importamos los paquetes necesarios
+import nltk
+import re
+import heapq  
+from googletrans import Translator
+from nltk import word_tokenize,sent_tokenize
+linea = "\n-----------------------------------------------------------------------------------"
+#Leemos el texto
+archTexto = open('texto.txt','r',encoding="utf8")
+contenido  = archTexto.read()
+archTexto.close()
+
+#Traducimos el texto ESP --> ENG
+translator = Translator()
+contenido_eng = translator.translate(str(contenido),dest="en", src="es")
+
+#Limpiamos el texto
+texto = re.sub(r'\[[0-9]*\]', ' ', contenido_eng.text) 
+texto = re.sub(r'\s+', ' ', contenido_eng.text)
+#texto = re.sub('[^a-zA-Z]', ' ',texto)
+texto = re.sub(r'\s+', ' ',texto)
+
+#Realizamos la tokenización de palabras y oraciones
+listaDePalabras = word_tokenize(texto)
+listaDeOraciones = sent_tokenize(texto)
+
+#Creamos una lista con las palabras más frecuentes del ingles
+palabrasFrec = nltk.corpus.stopwords.words('english')
+
+#Creamos una lista con la frecuencia de las palabras en el texto
+frecPalabras = {}
+for i in nltk.word_tokenize(texto):
+    if i not in palabrasFrec:
+        if i not in frecPalabras.keys():
+            frecPalabras[i] = 1
+        else:
+            frecPalabras[i] += 1
+
+#Buscamos la palabra con mayor frecuencia
+frecMax = max(frecPalabras.values())
+
+#Realizamos una normalización de las frecuencias
+for j in frecPalabras.keys():  
+    frecPalabras[j] = (frecPalabras[j]/frecMax)
+
+#Calculamos las frases que más se repiten
+puntajeOrac = {}  
+for sent in listaDeOraciones:  
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in frecPalabras.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in puntajeOrac.keys():
+                    puntajeOrac[sent] = frecPalabras[word]
+                else:
+                    puntajeOrac[sent] += frecPalabras[word]
+
+#Realizamos el resumen con las mejores frases
+resumenOrac = heapq.nlargest(15, puntajeOrac, key=puntajeOrac.get)
+resumen = ' '.join(resumenOrac)  
+
+#Traducir el texto ENG --> ESP
+translator = Translator()
+textoRes = translator.translate(resumen,dest="es", src="en")
+#print(type(textoRes.text))
+
+archResumen = open('resumen.txt','w')
+archResumen.write(textoRes.text) 
+archResumen.close()
+
